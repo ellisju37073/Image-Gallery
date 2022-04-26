4811e8b80ce22e9888736b4352b20fe441f6e43a
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2022-04-25 17:30:45 -0700

    update

--- home.php
+++ home.php
@@ -1,18 +1,31 @@
-<?php require "inc/functions.inc.php" ?>
-<!DOCTYPE html>
-<html lang="en">
-<!-- CTEC 127 / Bruce Elgort / February 2015 -->
-<head>
-	<meta charset="UTF-8">
-	<title>Justin Ellis Gallery</title>
-	<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+<?php
+// home.php
+session_start();
+$pageTitle = 'Home';
+require 'inc/header.inc.php';
+require "inc/functions.inc.php";
+?>
+<div class="container mt-3">
+    <div class="row">
+    <div class="col-12 col-md-12">
 
-	<h1>Ellis Image Gallery</h1>
+<a href="register.php">Register</a>
+<?php 
+    if(isset($_SESSION['loggedin'])){
+        echo '<a href="logout.php" id="logout">Logout</a>&nbsp;';
+    } else {
+        echo '<a href="login.php" id="login">Login</a>&nbsp;';
+    }
 
+?>
 
-	<?php 
+<h1>Welcome to Ellis Image Gallery <?= isset($_SESSION['first_name']) ? $_SESSION['first_name'] : 'New User! Please register and login to use the convertor' ?></h1>
+<?php 
+if(!isset($_SESSION['loggedin'])){
+    die();
+}
+?>
+<?php 
 		// check to see if file was uploaded
 		$message = processSubmittedFile();
 		//checks the error message for file uploads
@@ -22,12 +35,19 @@
 
 		require "inc/form.inc.html";
 	?>
-	<div class="container">
+	<div class="container-image">
 	<?php 
 		showImages('uploads');
 		showImages('vacation');
 		showImages('pets');
 	 ?>
 	 </div>
-</body>
-</html>
+
+</div> <!-- closing col -->
+</div> <!-- closing row -->
+</div> <!-- closing container div -->
+
+
+<script defer src="js/script.js"></script>
+
+<?php require 'inc/footer.inc.php' ?>
